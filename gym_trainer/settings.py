7b8cc9ae9d7a052d2d67139821d404e379889765
Hassan <hassanali4112@gmail.com>
2025-04-27 23:39:15 +0500

    added postgres config

--- gym_trainer/settings.py
+++ gym_trainer/settings.py
@@ -92,6 +92,9 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
+        "OPTIONS": {
+            "sslmode": "require",  # Neon requires SSL
+        },
     }
 }
 
